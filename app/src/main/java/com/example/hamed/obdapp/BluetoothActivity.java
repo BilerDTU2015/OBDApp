ac56d6e9fadd721bd218931fb657d49115e5200d
Fraktalen <niclasfa@gmail.com>
2015-05-06 15:08:26 +0200

    added dialog and connect to BluetoothSocket

--- app/src/main/java/com/example/hamed/obdapp/BluetoothActivity.java
+++ app/src/main/java/com/example/hamed/obdapp/BluetoothActivity.java
@@ -1,5 +1,12 @@
 package com.example.hamed.obdapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
@@ -9,12 +16,17 @@ import android.content.Context;
 
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
+
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,15 +34,20 @@ import android.widget.Toast;
 public class BluetoothActivity extends Activity implements OnClickListener {
 
     private static final int REQUEST_ENABLE_BT = 1;
+
     private Button onBtn;
     private Button offBtn;
     private Button listBtn;
     private Button findBtn;
+
     private TextView text;
     private BluetoothAdapter myBluetoothAdapter;
     private Set<BluetoothDevice> pairedDevices;
     private ListView myListView;
     private ArrayAdapter<String> BTArrayAdapter;
+
+    private String mDeviceIdentifier;
+    private String mDeviceName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,11 +83,77 @@ public class BluetoothActivity extends Activity implements OnClickListener {
 
             myListView = (ListView)findViewById(R.id.pairedListView);
             ArrayList<String> values = new ArrayList();
-            values.add("Paired devices");
+            //values.add("Paired devices");
+
+            // ListView Item Click Listener
+            myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+
+                    // ListView Clicked item index
+                    int itemPosition     = position;
+
+                    // ListView Clicked item value
+                    String  itemValue    = (String) myListView.getItemAtPosition(position);
+                    // Save device identifier for connection
+                    mDeviceIdentifier = itemValue;
+
+                    // Show Alert
+                    Toast.makeText(getApplicationContext(),
+                            "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                            .show();
+
+                    createConnectDialog();
+
+                }
+            });
 
             BTArrayAdapter = createAdapter(values);
             myListView.setAdapter(BTArrayAdapter);
         }
+    }
+
+    public void createConnectDialog(){
+
+        // split string into device name and device identifier
+        String[] splitted = mDeviceIdentifier.split("\\s+");
+        int len = splitted.length;
+        String uuidString = splitted[len - 1];
+        String name = "";
+        // Add every part of splitted except the last one which is uuid
+        for (String str : splitted){
+            if (str == uuidString){
+                break;
+            }
+            name += str;
+        }
+
+        // Connect to clicked device in list
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(BluetoothActivity.this);
+        //AlertDialog alert = new AlertDialog.Builder(getParent().this).create();
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage("uuid : " + uuidString)
+                .setTitle("Connect to " + name + "?");
+
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+
+        builder.setPositiveButton("Yes mein Führer", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton("NEIN NEIN NEIN!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+
+        builder.show();
     }
 
     @Override
@@ -88,6 +171,7 @@ public class BluetoothActivity extends Activity implements OnClickListener {
                 listPairedDevices();
                 break;
             case R.id.search:
+                discoverDevices();
                 break;
         }
     }
@@ -148,9 +232,21 @@ public class BluetoothActivity extends Activity implements OnClickListener {
     	}
     }
 
+    public void discoverDevices(){
+        if (myBluetoothAdapter.isDiscovering()) {
+            // the button is pressed when it discovers, so cancel the discovery
+            myBluetoothAdapter.cancelDiscovery();
+        }
+        else {
+            BTArrayAdapter.clear();
+            myBluetoothAdapter.startDiscovery();
+            registerReceiver(bReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        }
+    }
+
     // Create adapter for use in listPaired devices
     private ArrayAdapter<String> createAdapter(ArrayList<String> data){
-        return new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data.toArray(new String[data.size()]));
+        return new ArrayAdapter<String>(this, R.layout.bt_text_view, data.toArray(new String[data.size()]));
     }
 
     public void list(View view){
@@ -164,6 +260,17 @@ public class BluetoothActivity extends Activity implements OnClickListener {
         Toast.makeText(getApplicationContext(),"Show Paired Devices",
                 Toast.LENGTH_SHORT).show();
 
+    }
+
+    /*
+        Class that takes the string with device identifier and tries to create a bluetooth socket to the device and returns the socket.
+     */
+    public BluetoothSocket connectToOBD(String deviceAddress) throws Exception{
+        BluetoothDevice device = myBluetoothAdapter.getRemoteDevice(deviceAddress);
+        UUID uuid = UUID.fromString(deviceAddress);
+        BluetoothSocket socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+        socket.connect();
+        return socket;
     }
 
     final BroadcastReceiver bReceiver = new BroadcastReceiver() {
@@ -207,5 +314,6 @@ public class BluetoothActivity extends Activity implements OnClickListener {
         super.onDestroy();
         unregisterReceiver(bReceiver);
     }
+
 
 }
